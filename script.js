f7b88122c30c458c8298941e475bcae8f9777816
Emir Kadralija <43282886+EmirKadralija@users.noreply.github.com>
2023-01-18 10:12:40 +0000

    time add

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('My Script Loaded!');
+
+const getTime = () => {
+    const d = new Date();
+    const hours = d.getHours();
+    const minutes = d.getMinutes();
+    const seconds = d.getSeconds();
+    return document.getElementById('dateTime').innerHTML = hours + ":" + minutes + ":" + seconds;
+  }
+
+
+
+  setInterval(getTime, 1000);
